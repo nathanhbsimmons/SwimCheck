d2e9705f09fb481aac43c100d4252600e100a94c
Nathan Simmons <nate.simmons@malauzai.com>
2020-08-08 14:05:25 -0500

    Intital side menu architecture

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import {View,Text} from 'react-native';
+import {HelloWorld} from '_atoms';
+
+const App = () => <HelloWorld name="Helder Burato Berto" />;
+
+export default App;
